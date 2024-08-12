cb914455efe146b3ee34ab6bd8c5192008fa7e06
jejejeboooy <136441541+jejejeboooy@users.noreply.github.com>
2024-08-12 21:17:21 +0800

    v3 update
    Added:
    Appointment form
    Assessment Flow
    Donation
    
    Updated:
    About us section
    Footer icons
    
    Removed:
    Appointment.html

--- src/Atiman_frontend/assets/js/main.js
+++ src/Atiman_frontend/assets/js/main.js
@@ -1,24 +1,31 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+'use strict'
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission if validation fails
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
 })
-{
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
+
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("phoneNumber");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text (optional) */
+  alert("Copied the text: " + copyText.value);
 }
